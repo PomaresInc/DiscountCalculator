d1e148dd0ed8ef9f3df089e4ba71ef34d3741989
Javier <javpomgom@alu.edu.gva.es>
2026-01-30 20:24:12 +0100

    Changin code logic

--- app/src/main/java/dam/optativa/DiscountCalculator.java
+++ app/src/main/java/dam/optativa/DiscountCalculator.java
@@ -2,14 +2,14 @@ package dam.optativa;
 
 public class DiscountCalculator {
     public double calcular(double price, boolean isStudent){
-        if (price >= 0){
-            return 0;
+        if (price <= 0){
+            return 0;  
         }else{
-            if (isStudent == true){
-                return price * 0.8;
+            if (isStudent){
+                return price * 0.8;  
             }else{ 
-                return price * 0.95;
-                }
+                return price * 0.95;  
             }
         }
     }
+}
